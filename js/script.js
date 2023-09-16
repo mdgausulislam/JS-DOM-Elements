fe0a5d21d65b6e21ff6c127891dbc60113fb7fa5
unknown <gausulislam5@gmail.com>
2023-09-16 15:48:47 +0600

    js dom

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log("hello js");
+console.log(document);
+
+const student={
+    name:'Hemonto',
+    id:191002325,
+    study:function(time){
+        console.log(time,'js programming started');
+    }
+}
