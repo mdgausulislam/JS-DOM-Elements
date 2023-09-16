108095cf187f77f377d19167642e4e612e58baa5
unknown <gausulislam5@gmail.com>
2023-09-16 17:17:22 +0600

    dom update

--- js/script.js
+++ js/script.js
@@ -18,6 +18,19 @@ for (const i of liCollection) {
 const allHeadings=document.getElementsByTagName('li');
 for (const i of allHeadings) {
     console.log(i);
-    console.log(i.innerText);
+    console.log(i.innerText); 
+}
+
+const placesTitle=document.getElementById('places-title')
+console.log(placesTitle);
+console.log(placesTitle.innerText="my tour list");
+const places=document.getElementsByClassName('important-places');
+console.log(places);
+console.log(places.innerText);
+for (const place of places) {
+    console.log(place);
+    console.log(place.innerText);
     
 }
+const otherPlace=document.getElementById('other-place');
+console.log(otherPlace);
