7ed4ad5cd41da5ce176176d6521ae9c6e59bb4b0
unknown <gausulislam5@gmail.com>
2023-09-16 16:01:51 +0600

    dom update

--- js/script.js
+++ js/script.js
@@ -8,3 +8,16 @@ const student={
         console.log(time,'js programming started');
     }
 }
+const liCollection=document.getElementsByTagName('li');
+// console.log(liconection);
+
+for (const i of liCollection) {
+    console.log(i.innerText);
+    
+}
+const allHeadings=document.getElementsByTagName('li');
+for (const i of allHeadings) {
+    console.log(i);
+    console.log(i.innerText);
+    
+}
